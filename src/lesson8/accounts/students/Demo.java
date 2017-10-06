5467a813881bb0214a25fd1062ef51f3ef072e7c
iomel <igor.omel@gmail.com>
2017-10-06 22:00:20 +0200

    lesson 8 Student update

--- src/lesson8/accounts/students/Demo.java
+++ src/lesson8/accounts/students/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
         return new Student("Vasya", "Pupkin", 312, null);
     }
 
-    Student createLowestChild ()
+    SpecialStudent createLowestChild ()
     {
         return new SpecialStudent("Ferdya", "Lozhkin", 313, null, 222333222, "specstud@gmail.com");
     }
