a3c563516ee928a3d2c8046e6a4789890d6e5aa6
iomel <igor.omel@gmail.com>
2017-10-06 22:39:58 +0200

    Les 9 User class

---
+++ src/lesson9/UserRepository.java
@@ -0,0 +1,40 @@
+package lesson9;
+
+public class UserRepository {
+    private User[] users;
+
+    public UserRepository(User[] users) {
+        this.users = users;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public String[] getUserNames()
+    {
+        String nameList = new String[users.length];
+        for(int i = 0; i < users.length; i++)
+            nameList[i] = users[i].getName();
+        return nameList;
+    }
+
+    public long[] getUserIds()
+    {
+        long idList = new long[users.length];
+        for(int i = 0; i < users.length; i++)
+            idList[i] = users[i].getId();
+        return idList;
+    }
+
+    public String getUserNameById(long id)
+    {
+        for(User user : users)
+            if(user.getId() == id)
+                return user.getName();
+
+        return "No such user";
+    }
+
+
+}
