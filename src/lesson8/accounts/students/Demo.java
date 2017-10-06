026c648bc864a85b4329fb8972f99930f7061911
iomel <igor.omel@gmail.com>
2017-10-06 22:25:03 +0200

    lesson 8 Student update

--- src/lesson8/accounts/students/Demo.java
+++ src/lesson8/accounts/students/Demo.java
@@ -1,15 +1,22 @@
 package lesson8.accounts.students;
 
+import java.util.Date;
+
 public class Demo {
 
     public static Student createHighestParent()
     {
 
-        return new Student("Vasya", "Pupkin", 312, null);
+        Course cur = new Course(new Date(), "Math", 20, "Petrovich", null);
+        Course[] curList = {cur};
+        return new Student("Vasya", "Pupkin", 312, curList);
     }
 
     public static SpecialStudent createLowestChild ()
     {
-        return new SpecialStudent("Ferdya", "Lozhkin", 313, null, 222333222, "specstud@gmail.com");
+        Course cur = new Course(new Date(), "Math", 20, "Petrovich", null);
+        Course[] curList = {cur};
+
+        return new SpecialStudent("Ferdya", "Lozhkin", 313, curList, 222333222, "specstud@gmail.com");
     }
 }
