9322f64c391549a0cdb21e8137abfda32e7c429e
iomel <igor.omel@gmail.com>
2017-10-07 12:08:51 +0200

    Les 9 users Empl

--- src/lesson9/employee/utils/Demo1.java
+++ src/lesson9/employee/utils/Demo1.java
@@ -1,6 +1,6 @@
 package lesson9.employee.utils;
 
-public class Demo {
+public class Demo1 {
     protected static void main(String[] args) {
         Checker checker = new Checker();
         System.out.println(checker.companyNamesValidatedCount);
