020e923993bdcaff10c5d42bc6bb527dd92918b8
iomel <igor.omel@gmail.com>
2017-10-07 12:07:38 +0200

    Les 9 users Empl

--- src/lesson9/employee/Demo.java
+++ src/lesson9/employee/Demo.java
@@ -4,15 +4,14 @@ import lesson9.employee.utils.Checker;
 
 public class Demo {
     public static void main(String[] args) {
-        Company company = new Company();
+        Company company = new Company("KJD", "Vietnam");
 
-        System.out.println(company.countryFounded);
-        System.out.println(company.name);
+        System.out.println(company.getCountryFounded());
+        System.out.println(company.getName());
 
-        company.name = "IBM";
 
         Checker checker = new Checker();
-        System.out.println(checker.checkCompanyName(company.name));
+        System.out.println(checker.checkCompanyName(company.getName()));
         System.out.println(checker.companyNamesValidatedCount);
 
 
--- src/lesson9/employee/utils/Demo.java
+++ src/lesson9/employee/utils/Demo.java
@@ -1,7 +1,7 @@
 package lesson9.employee.utils;
 
 public class Demo {
-    public static void main(String[] args) {
+    protected static void main(String[] args) {
         Checker checker = new Checker();
         System.out.println(checker.companyNamesValidatedCount);
 
