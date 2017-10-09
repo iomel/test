86437d8791bfbee62e6fd2b04dd7bba5fe2aa7b7
iomel <igor.omel@gmail.com>
2017-10-09 17:30:22 +0200

    Les 12 Bank  paySalary

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -55,9 +55,7 @@ public class UkrainianBankSystem implements BankSystem {
 
     @Override
     public void paySalary(User user) {
-        if (user == null)
-            return;
-        if ((user.getBalance() + user.getSalary() > user.getBank().getLimitOfFunding()))
+        if ((user.getBalance() + user.getSalary() >= user.getBank().getLimitOfFunding()))
             return;
         user.setBalance((user.getBalance() + user.getSalary()));
 
